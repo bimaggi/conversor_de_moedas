34bd56068e07d20e281e28c7a7571c1349ff96f1
bimaggi <bmg.olivier@gmail.com>
2021-11-11 14:44:55 -0300

    alteraçao footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from "react"
 function Footer(){
     return(
         <footer>
-            <p><a href="https://www.linkedin.com/in/marcia-bimaggi-marcia-beserra-de-oliveira-9303801b9/"><strong>Copyright&copy 2021; Created and powered by Marcia Bimaggi</strong> </a></p>  
+            <p><a href="https://www.linkedin.com/in/marcia-b-oliveira/"><strong>Copyright&copy 2021; Created and powered by Marcia Bimaggi</strong> </a></p>  
         </footer> 
     )
 }
